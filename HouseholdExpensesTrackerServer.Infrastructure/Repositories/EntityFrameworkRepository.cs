60548c987e341e2ed4f01219e59e16b2fa909184
Hodorek, Tomasz <tomasz.hodorek@capgemini.com>
2018-05-02 07:36:00 +0200

    all methods are virtual now

--- HouseholdExpensesTrackerServer.Infrastructure/Repositories/EntityFrameworkRepository.cs
+++ HouseholdExpensesTrackerServer.Infrastructure/Repositories/EntityFrameworkRepository.cs
@@ -24,85 +24,85 @@ namespace HouseholdExpensesTrackerServer.Infrastructure.Repositories
             _dbSet = _context.Set<TModel>();
         }
 
-        public void Add(TModel entity)
+        public virtual void Add(TModel entity)
         {
             _dbSet.Add(entity);
         }
 
-        public async Task AddAsync(TModel entity, 
+        public virtual async Task AddAsync(TModel entity, 
             CancellationToken cancellationToken = default(CancellationToken))
         {
             await _dbSet.AddAsync(entity, cancellationToken); 
         }
 
-        public void Delete(TModel entity)
+        public virtual void Delete(TModel entity)
         {
             _dbSet.Remove(entity);
         }
 
-        public void DeleteById(UIdentifier id)
+        public virtual void DeleteById(UIdentifier id)
         {
             var entity = this.GetById(id);
             this.Delete(entity);
         }
 
-        public async Task DeleteByIdAsync(UIdentifier id, 
+        public virtual async Task DeleteByIdAsync(UIdentifier id, 
             CancellationToken cancellationToken = default(CancellationToken))
         {
             var entity = await this.GetByIdAsync(id, cancellationToken);
             this.Delete(entity);
         }
 
-        public TModel Find(Expression<Func<TModel, bool>> predicate)
+        public virtual TModel Find(Expression<Func<TModel, bool>> predicate)
         {
             return _dbSet.SingleOrDefault(predicate);
         }
 
-        public ICollection<TModel> FindAll(Expression<Func<TModel, bool>> predicate)
+        public virtual ICollection<TModel> FindAll(Expression<Func<TModel, bool>> predicate)
         {
             return _dbSet.Where(predicate).ToList();
         }
 
-        public async Task<ICollection<TModel>> FindAllAsync(Expression<Func<TModel, bool>> predicate, 
+        public virtual async Task<ICollection<TModel>> FindAllAsync(Expression<Func<TModel, bool>> predicate, 
             CancellationToken cancellationToken = default(CancellationToken))
         {
             return await _dbSet.Where(predicate).ToListAsync(cancellationToken);
         }
 
-        public async Task<TModel> FindAsync(Expression<Func<TModel, bool>> predicate, 
+        public virtual async Task<TModel> FindAsync(Expression<Func<TModel, bool>> predicate, 
             CancellationToken cancellationToken = default(CancellationToken))
         {
             return await _dbSet.SingleOrDefaultAsync(predicate, cancellationToken);
         }
 
-        public ICollection<TModel> GetAll()
+        public virtual ICollection<TModel> GetAll()
         {
             return _dbSet.ToList();
         }
 
-        public async Task<ICollection<TModel>> GetAllAsync(
+        public virtual async Task<ICollection<TModel>> GetAllAsync(
             CancellationToken cancellationToken = default(CancellationToken))
         {
             return await _dbSet.ToListAsync(cancellationToken);
         }
 
-        public TModel GetById(UIdentifier id)
+        public virtual TModel GetById(UIdentifier id)
         {
             return _dbSet.Find();
         }
 
-        public async Task<TModel> GetByIdAsync(UIdentifier id, 
+        public virtual async Task<TModel> GetByIdAsync(UIdentifier id, 
             CancellationToken cancellationToken = default(CancellationToken))
         {
             return await _dbSet.FindAsync(id, cancellationToken);
         }
 
-        public int SaveChanges()
+        public virtual int SaveChanges()
         {
             return _context.SaveChanges();
         }
 
-        public async Task<int> SaveChangesAsync(
+        public virtual async Task<int> SaveChangesAsync(
             CancellationToken cancellationToken = default(CancellationToken))
         {
             return await _context.SaveChangesAsync(cancellationToken);
